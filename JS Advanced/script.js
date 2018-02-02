301547a609414dd07652250d95e6d9dcc99cdf97
ulyanat <trushkovaulyana@gmail.com>
2018-02-03 02:41:47 +0400

    Update script.js

--- JS Advanced/script.js
+++ JS Advanced/script.js
@@ -9,6 +9,33 @@ iii.	1 если 1-е число больше 2-го
 c.	[Дополнительно] Если одно из чисел не передано или передено не число, то должна быть проброшена ошибка, информирующая об этом (в setTimeout) и перехвачена снаружи метода (используйте методы Promise)
 */
 
+function compare(value1, value2){
+	var promise = new Promise(function(resolve, reject) {
+		setTimeout(function(){ 
+		if ( (typeof(value1) == 'number') && (typeof(value2) == 'number') ){
+			resolve("result"); 
+		} else reject (new Error ('Введите два числа'));
+		}, 1000);
+	}); 
+
+	promise 
+		.then( 
+			function(result){ 
+			if (value1 < value2){console.log(-1);}; 
+			if (value1 > value2){console.log(1);}; 
+			if (value1 == value2){console.log(0);}; 
+		},) 
+ 
+		.catch(
+			function(err){
+			console.log(err.message);
+		},);
+} 
+console.log (compare(1, 3));
+console.log (compare(3, 3));
+console.log (compare(5, 3));
+console.log (compare(3));
+
 /*
 2.	Цепочка промисов:
 Для задания 'a':
@@ -33,6 +60,49 @@ a.	Используя 'random' напишите цепочку промисов,
 b.	Используя 'random' и методы Promise выведите в массиве результаты выполнения этой функции 7 раз.
 */
 
+// 2.a
+function random(sumWith) {
+    return new Promise(function(resolve) {
+      var timeout = Math.random()*3000;
+      setTimeout(function(){
+        resolve(Math.random()*3 + sumWith);
+    }, timeout)
+  })
+}
+
+random(5)
+.then(
+	function(result){
+		return (random(result));
+	})
+.then(
+	function(result){
+		return (random(result));
+	})
+.then(
+	function(result){
+		return console.log(result);
+	});
+
+// 2.b
+
+function random() {
+    return new Promise(function(resolve) {
+      var timeout = Math.random()*3000;
+      setTimeout(function(){
+        resolve(Math.random()*3);
+    }, timeout)
+  })
+}
+
+var arr  = [];
+for(var i = 0; i < 7; i++){
+	arr.push(random());
+}
+
+Promise.all(arr).then(function(results){
+    console.log(results);
+});
 
 /*
 3.	Замыкания:
@@ -115,9 +185,32 @@ var number = 2;
 var double = sumWith.bind({currentValue: 3});
 alert(double(number));
 
+//**********************
+
+function sumWith(number) {
+	return this.currentValue += number;
+}
+var number = 2;
+alert(sumWith.call({currentValue: 3}, number));
+
 //[Дополнительно] Создать метод на основе 'sumWIth', первый вызов которого будет возвращать 1, а все последующие на 2 больше (например, 1, 3, 5, 7...).
 
+function sumWith(number) {
+	return this.currentValue += number;
+}
 
+function sum(){
+	var value = {currentValue: -1};
+	return function(){
+		return sumWith.bind(value)(2);
+	};
+}
+var counter = sum();
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
 
 /*
 5.	setInterval:
@@ -125,15 +218,25 @@ alert(double(number));
 [Дополнительно] Напишите код, который будет выводить текст через 1 секунду, потом через 3 после предыдущего, потом через 5 после предыдущего, потом через 7  после предыдущего и далее через 9 секунд после предыдущего.
 */
 
-var timerId = setInterval(function() {
-  alert( "Текст" );
-}, 2000);
-
-setTimeout(function() {
-  clearInterval(timerId);
-}, 10001);
+function timer() {
+	var i = 1;
+	var timerId = setInterval(function() {
+		alert('текст');
+		if (i == 5) clearInterval(timerId);
+		i++;
+	}, 2000);
+}
+timer();
 
 //[Дополнительно] Напишите код, который будет выводить текст через 1 секунду, потом через 3 после предыдущего, потом через 5 после предыдущего, потом через 7  после предыдущего и далее через 9 секунд после предыдущего.
 
-
-
+function timer() {
+	var i = 1000;
+	setTimeout(function run() {
+		alert('текст');
+		i += 2000;
+		if (i > 9000) return;	
+		setTimeout(run, i);
+	}, i);
+}
+timer();
